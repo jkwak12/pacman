edcec3463d180fcf16734def524f9814baf25205
mchen1994 <mchen1994@gmail.com>
2016-09-23 17:52:04 -0500

    Update print_solutions.py

--- print_solutions.py
+++ print_solutions.py
@@ -4,26 +4,35 @@ import pdb
 
 
 #assume the list 'dots' is already in the order they were eaten in
-def write_dots_eaten(maze, dots):
+#also added back in the printing of dots along the path since a TA in office hours
+#		said that they wanted the visible path alongside the order of dots eaten
+def write_dots_eaten(maze, dots, path):
 	f = open('solution.txt','w+')
+	start = path[0]
+	goal = path[len(path) - 1]
 	for i in range(len(maze)):
 		for j in range(len(maze[0])):
-			f.write(maze[i][j])
-		f.write('\n')
-	'''done writing initial maze state'''	
+			if (i,j) in path and not (i,j) == start and not (i,j) == goal:
+				f.write('.')
+			else:
+				f.write(Maze[i][j])
+		f.write('\n')	
+	'''done writing maze state with path of dots'''
+
+	'''now including the order of initial dots eaten'''	
 	f.seek(0,0)	#move file cursor to beginning
 	contents = f.read()
 	count = 48		#ascii value for '0'
 	for dot in dots:
 		x = dot[1]	#column value 
 		y = dot[0]	#row value
-		index = y*(len(maze[0]) + 1) + x	#some crazy offset, just take for granted ;)
-		contents = contents[:index] + chr(count) + contents[index+1:]	
+		index = y*(len(maze[0]) + 1) + x
+		contents = contents[:index] + chr(count) + contents[index+1:]	#some crazy offset
 		count += 1
 		if(count == 58): 	#is this ascii value '10'?
 			count = 97		#ascii value for 'a'
 		elif(count == 123):	#is this value greater than alphabet length?
 			count = 65		#ascii value for 'A'
 	f.seek(0,0)
-	f.write(contents)		
+	f.write(contents)
 	f.close()
